624644d422a4e77a70ff737e87c40b8ce865c3ee
alik <qurbonovalijon30@gmail.com>
2024-10-23 17:32:49 +0500

    updating 2

--- exchange_rate/urls.py
+++ exchange_rate/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from . import views
+from .views import *
 
 urlpatterns = [
     path("", home, name = "home"),
+    path('currencies/', currency_list, name='currency_list'),
+    path('exchange-rates/', exchange_rate_list, name='exchange_rate_list'),
+    path('historical-rates/', historical_exchange_rate_list, name='historical_exchange_rate_list'),
 ]
--- exchange_rate/views.py
+++ exchange_rate/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Currency, ExchangeRate, HistoricalExchangeRate
 
 def home(request):
     template = loader.get_template("home.html")
     return HttpResponse(template.render())
+
+def currency_list(request):
+    currencies = Currency.objects.all()
+    return render(request, 'test1.html', {'currencies': currencies})
+
+def exchange_rate_list(request):
+    exchange_rates = ExchangeRate.objects.all()
+    return render(request, 'test2.html', {'exchange_rates': exchange_rates})
+
+def historical_exchange_rate_list(request):
+    historical_rates = HistoricalExchangeRate.objects.all()
+    return render(request, 'test2.html', {'historical_rates': historical_rates})
